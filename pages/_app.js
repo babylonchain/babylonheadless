089e25a5914d2bf92487c89a5267ded49efb1a65
rupak <rupak.shrestha@ebpearls.com>
2023-05-23 14:58:11 +0545

    added new favicon icom

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,18 @@ import { ApolloProvider } from "@apollo/client/react";
 import client from "../src/apollo/client";
 import "aos/dist/aos.css";
 import "../styles/index.scss";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   // console.log(client);
   return (
     <ApolloProvider client={client}>
+      <Head>
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+    </Head>
       <Component {...pageProps} />
     </ApolloProvider>
   );
